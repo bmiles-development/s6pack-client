741dcfa5d88e3f8d3321990bf2fc637bc238953d
bmilesp <bmilesp@gmail.com>
2024-11-24 17:19:59 -0500

    init

--- src/store/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import menu from './menu';
-import plan from './plan';
-import snackBarMessages from './snackBarMessages';
-// ==============================|| COMBINE REDUCERS ||============================== //
-
-const reducers = combineReducers({ menu, plan, snackBarMessages });
-
-export default reducers;
